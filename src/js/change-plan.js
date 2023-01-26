74e27934f49869d241c79f5feaeaea0f3fb59986
JC <jc.alonsome@gmail.com>
2023-01-26 18:06:45 +0100

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- src/js/change-plan.js
+++ src/js/change-plan.js
@@ -1,19 +1,16 @@
-const mainContainer = document.getElementById('main-container');
+const mainContainer = document.getElementById("main-container");
 
-const plans = ['basic', 'proffesional', 'yearly'];
+const plans = ["basic", "professional", "master"];
 
 const togglePlan = (PRICING_OPTIONS, optionInput) => {
-  const basic = mainContainer.children[0].children[1];
-  const monthly = Object.values(PRICING_OPTIONS.monthly.basic);
-  let possiblePlans;
   for (let index = 0; index < 3; index++) {
-    possiblePlans = mainContainer.children;
     for (let j = 0; j < 4; j++) {
       if (optionInput.checked) {
-        console.log(
-          (mainContainer.children[index].children[1].children[j].textContent =
-            Object.values(PRICING_OPTIONS.yearly[plans[index]][j]))
-        );
+        mainContainer.children[index].children[1].children[j].textContent =
+          Object.values(PRICING_OPTIONS.yearly[plans[index]])[j];
+      } else {
+        mainContainer.children[index].children[1].children[j].textContent =
+          Object.values(PRICING_OPTIONS.monthly[plans[index]])[j];
       }
     }
   }
--- src/js/const.js
+++ src/js/const.js
@@ -1,44 +1,44 @@
 const PRICING_OPTIONS = {
   monthly: {
     basic: {
-      price: 19.99,
-      storage: 500,
-      users: 2,
-      sendUp: 3
+      price: "$19.99",
+      storage: "500gb",
+      users: "2 Users Allowed",
+      sendUp: "Send up to 3GB",
     },
     professional: {
-      price: 24.99,
-      storage: 1,
-      users: 5,
-      sendUp: 10
+      price: "$24.99",
+      storage: "1tb",
+      users: "10 Users Allowed",
+      sendUp: "Send up to 10GB",
     },
     master: {
-      price: 39.99,
-      storage: 2,
-      users: 10,
-      sendUp: 20
-    }
+      price: "$39.99",
+      storage: "2tb",
+      users: "20 Users Allowed",
+      sendUp: "Send up to 20GB",
+    },
   },
   yearly: {
     basic: {
-      price: 199.99,
-      storage: 800,
-      users: 2,
-      sendUp: 5
+      price: "$199.99",
+      storage: "2tb",
+      users: "10 Users Allowed",
+      sendUp: "Send up to 5GB",
     },
     professional: {
-      price: 249.99,
-      storage: 2,
-      users: 5,
-      sendUp: 15
+      price: "$249.99",
+      storage: "5tb",
+      users: "20 Users Allowed",
+      sendUp: "Send up to 10GB",
     },
     master: {
-      price: 399.99,
-      storage: 3,
-      users: 10,
-      sendUp: 25
-    }
-  }
+      price: "$299.99",
+      storage: "10tb",
+      users: "30 Users Allowed",
+      sendUp: "Send up to 20GB",
+    },
+  },
 };
 
 export { PRICING_OPTIONS };
